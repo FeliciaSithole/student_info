d70b45f53732161808f8858aadb8c3e7a85d57fc
FeliciaSithole <feliciasithole.fs@gmail.com>
2026-07-16 23:56:14 +0200

    added the student name, surname and age

---
+++ student_info.py
@@ -0,0 +1,9 @@
+name = input("What is your name? ")
+surname = input("What is your surname? ")
+
+print(f"Welcome, " + name + surname)
+
+birth_year = input("Enter your birth year: ")
+age = 2026 - int(birth_year)
+print(age)
+
