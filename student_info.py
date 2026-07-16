827def43247148152cd4197f0e66454489b664e6
FeliciaSithole <feliciasithole.fs@gmail.com>
2026-07-16 23:59:18 +0200

    added upper case and title case

--- student_info.py
+++ student_info.py
@@ -1,7 +1,12 @@
 name = input("What is your name? ")
 surname = input("What is your surname? ")
 
-print(f"Welcome, " + name + surname)
+full_name = f"{name} {surname}"
+
+print(f"Welcome, {full_name}!")
+
+print(f"Upper Case: {full_name.upper()}")
+print(f"Title Case: {full_name.title()}")
 
 birth_year = input("Enter your birth year: ")
 age = 2026 - int(birth_year)
